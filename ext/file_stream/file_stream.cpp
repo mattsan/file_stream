20c97d6c5991ca69a71ea3734ce2a2bd58c8289f
Eiji MATSUMOTO <e.mattsan@gmail.com>
2016-04-29 16:37:36 +0900

    add read

--- ext/file_stream/file_stream.cpp
+++ ext/file_stream/file_stream.cpp
@@ -12,10 +12,11 @@ namespace
 
 struct Stream
 {
-    std::ifstream* source;
+    std::istream* source;
 
     static Stream* open(const std::string& filename)
     {
+        std::cout << "open " << filename << std::endl;
         Stream* stream = new Stream;
         stream->source = new std::ifstream(filename);
         return stream;
@@ -25,6 +26,7 @@ struct Stream
     {
         delete stream->source;
         delete stream;
+        std::cout << "closed" << std::endl;
     }
 };
 
@@ -41,11 +43,29 @@ VALUE file_stream_close(VALUE self)
     return self;
 }
 
+VALUE file_stream_readline(VALUE self)
+{
+    Stream* stream;
+    Data_Get_Struct(self, Stream, stream);
+    std::string s;
+    if(std::getline(*stream->source, s).good())
+    {
+        std::cout << "read " << s << std::endl;
+        return rb_str_new2(s.c_str());
+    }
+    else
+    {
+        std::cout << "no good" << std::endl;
+        return Qnil;
+    }
+}
+
 void Init_file_stream()
 {
     VALUE stream = rb_define_class("FileStream", rb_cObject);
     rb_define_singleton_method(stream, "open", RUBY_METHOD_FUNC(file_stream_open), 1);
     rb_define_method(stream, "close", RUBY_METHOD_FUNC(file_stream_close), 0);
+    rb_define_method(stream, "readline", RUBY_METHOD_FUNC(file_stream_readline), 0);
 }
 
 } // namespace
